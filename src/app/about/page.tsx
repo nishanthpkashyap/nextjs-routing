3adf2ed30c65009b0dd51b102f5e30bb0c461a05
nishanth.p <nishath.p@geekyants.com>
2023-10-26 16:20:15 +0530

    page based routing

---
+++ src/app/about/page.tsx
@@ -0,0 +1,7 @@
+export default function About(){
+    return (
+        <div className="flex justify-center items-center">
+            <h1>About Us</h1>
+        </div>
+    )
+}
---
+++ src/app/profile/page.tsx
@@ -0,0 +1,7 @@
+export default function Profile(){
+    return (
+        <div className="flex justify-center items-center">
+            <h1>Profile</h1>
+        </div>
+    )
+}
